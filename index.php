690bec61f645e240a843fabf52a74daf33765bcd
anthony39100 <anthonybreviglieri.ab@gmail.com>
2020-01-07 13:02:28 +0000

    ajout du routeur

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 
-
+<?php
+$url=explode("/",$_SERVER['REQUEST_URI']);
+switch ($url[1]) {
+    case  "film":
+        include('controllers/indexController.php');
+        break;
+    case "categorie":
+        echo "i égal 1";
+        break;
+        case "":
+            break;
+    case 'annees':
+        echo "i égal 2";
+        break;
+    case 'annees':
+        echo "i égal 2";
+        break;
+    default:
+        echo 'page not found';
+    
+}
+?>
 
 
 
