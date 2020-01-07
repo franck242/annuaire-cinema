0b6bbc0918221ff37130b2e3a919ea4e03f30a32
sylvain <sylvain.santos@neuf.fr>
2020-01-07 13:59:29 +0000

    last commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
-var_dump ($request);
-switch ($request) {
+$request = explode('/',$_SERVER['REQUEST_URI']);
+// var_dump ($request);
+switch ($request[1]) {
     case '/' :
         require __DIR__ . '/views/index.php';
         break;
@@ -16,6 +16,7 @@ switch ($request) {
         http_response_code(404);
         require __DIR__ . '/views/404.php';
         break;
+
 }
 
 
